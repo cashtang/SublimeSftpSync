eff8df567f1a3bf0911f1bb2149f1c94b3fed0de
Tang Cheng <cheng.tang@supwisdom.com>
2012-12-25 15:43:22 +0800

    修改路径问题

--- SftpSync.py
+++ SftpSync.py
@@ -1,10 +1,11 @@
-import sublime, sublime_plugin
+import sublime
+import sublime_plugin
 from sftpsync_module import PSCPCommand
 import functools
 
 
 class SftpSyncCommand(sublime_plugin.WindowCommand):
-    def run(self,upload = True,allopenfile = False,encoding='utf-8'):
+    def run(self, upload=True, allopenfile=False, encoding='utf-8'):
         self.encoding = encoding
         #sublime.message_dialog(u'enter sftpsync plugin')
         if allopenfile:
@@ -14,36 +15,35 @@ class SftpSyncCommand(sublime_plugin.WindowCommand):
             self.window.run_command("show_panel", {"panel": "output.sftpsync"})
             views = self.window.views()
             for view in views:
-                self._sync_file(upload,view)
+                self._sync_file(upload, view)
         else:
             self.output_view = self.window.get_output_panel('sftpsync')
             self.window.run_command("show_panel", {"panel": "output.sftpsync"})
             view = self.window.active_view()
-            self._sync_file(upload,view)
+            self._sync_file(upload, view)
 
-    def _sync_file(self,upload,view):       
+    def _sync_file(self, upload, view):
         if view.is_dirty():
             if sublime.ok_cancel_dialog(u'Save file [%s] first?' % view.file_name()):
                 sublime.run_command("save")
                 if view.is_dirty():
-                    sublime.message_dialog(u'Save file failed!')
+                    sublime.message_dialog(u'Save file failed!!')
                     return
 
-        #sublime.ok_cancel_dialog(u'Save file %s!' % view.file_name())
         pscp = PSCPCommand(self)
-        pscp.transferFile(view.file_name(),upload)
-        #     self._append_data(u"SftpSync file %s error" % view.name())            
+        pscp.transferFile(view.file_name(), upload)
+        #     self._append_data(u"SftpSync file %s error" % view.name())
 
-    def append_data(self,msg):
+    def append_data(self, msg):
         try:
             msg = msg.decode(self.encoding)
         except:
             msg = 'Output is not encoding ' + self.encoding
         self.output_view.set_read_only(False)
         edit = self.output_view.begin_edit()
-        self.output_view.insert(edit,self.output_view.size(),msg + '\n')
+        self.output_view.insert(edit, self.output_view.size(), msg + '\n')
         self.output_view.end_edit(edit)
-        self.output_view.set_read_only(True) 
+        self.output_view.set_read_only(True)
 
     def finish(self, proc):
         self.window.run_command("show_panel", {"panel": "output.sftpsync"})
@@ -61,4 +61,3 @@ class SftpSyncCommand(sublime_plugin.WindowCommand):
 
     def on_finished(self, proc):
         sublime.set_timeout(functools.partial(self.finish, proc), 0)
-        
--- sftpsync_module.py
+++ sftpsync_module.py
@@ -1,20 +1,20 @@
 #!/usr/bin/env
 # -*- coding: utf-8 -*-
-import os,sys
+import os
+import sys
 import ConfigParser
 import subprocess
 import thread
 import time
 import shlex
 import sublime
-CFGFILENAME='sftpcfg.ini'
+CFGFILENAME = 'sftpcfg.ini'
+
 
 def _get_plugin_path():
-    plugin =  os.path.realpath(sublime.packages_path())
-    plugin = os.path.join(plugin,"sftpsync")
+    plugin = os.path.realpath(sublime.packages_path())
+    plugin = os.path.join(plugin, "SublimeSftpSync")
     return plugin
-    
-
 
 #PLUGINPATH = _get_plugin_path()
 
@@ -75,7 +75,7 @@ class AsyncProcess(object):
 
     def read_stdout(self):
         while True:
-            data = os.read(self.proc.stdout.fileno(), 2**15)
+            data = os.read(self.proc.stdout.fileno(), 2 ** 15)
 
             if data != "":
                 if self.listener:
@@ -88,7 +88,7 @@ class AsyncProcess(object):
 
     def read_stderr(self):
         while True:
-            data = os.read(self.proc.stderr.fileno(), 2**15)
+            data = os.read(self.proc.stderr.fileno(), 2 ** 15)
 
             if data != "":
                 if self.listener:
@@ -96,20 +96,20 @@ class AsyncProcess(object):
             else:
                 self.proc.stderr.close()
                 break
-                
+
 
 class PSCPCfg:
     def __init__(self):
-        self.svrip=''
-        self.svrport=22
-        self.srcfile=''
-        self.svruser='root'
-        self.svruserpwd=''
-        self.svrkeyfile=''
-        self.svrbasepath='/'
+        self.svrip = ''
+        self.svrport = 22
+        self.srcfile = ''
+        self.svruser = 'root'
+        self.svruserpwd = ''
+        self.svrkeyfile = ''
+        self.svrbasepath = '/'
         self.timeout = 30
 
-    def load_config(self,cfgfile):
+    def load_config(self, cfgfile):
         parser = ConfigParser.RawConfigParser()
         parser.read(cfgfile)
         cfgs = dict(parser.items('BASE'))
@@ -123,53 +123,52 @@ class PSCPCfg:
         self.svrbasepath = cfgs.get('svrbasepath')
         self.timeout = cfgs.get('timeout', 10)
         if len(self.svrbasepath) == 0:
-            self.svrbasepath='/'
-        
+            self.svrbasepath = '/'
+
 
 class PSCPCommand:
-    def __init__(self,listener):
-        self.pscp_cfg_=PSCPCfg()
-        self.relative_path_=''
+    def __init__(self, listener):
+        self.pscp_cfg_ = PSCPCfg()
+        self.relative_path_ = ''
         self.listener = listener
         self.proc = None
 
-
-    def _findCfgForPath(self,dirpath):
+    def _findCfgForPath(self, dirpath):
         """ 检查路径 sftp.ini 配置文件
         """
         checkpath = dirpath
         self.relative_path_ = ''
         while 1:
-            cfgdir = os.path.join(checkpath,CFGFILENAME)
+            cfgdir = os.path.join(checkpath, CFGFILENAME)
             if os.path.isfile(cfgdir):
                 return cfgdir
             pathsplit = os.path.split(checkpath)
             # root dir
             if pathsplit[0] == checkpath:
                 return ""
-            checkpath=pathsplit[0]
-            self.relative_path_=os.path.join(pathsplit[1],self.relative_path_)
-            
+            checkpath = pathsplit[0]
+            self.relative_path_ = os.path.join(pathsplit[1], self.relative_path_)
+
         return ""
-                
-    def showResult(self,msg):
+
+    def showResult(self, msg):
         #cmdline = 'net send "%s" %s ' % (os.environ['COMPUTERNAME'],msg)
         #p = subprocess.Popen(cmdline)
         print msg
-        
-    def execUpload(self,filepath):
+
+    def execUpload(self, filepath):
         #if len(self.relative_path_) == 0:
         #    self.relative_path_ = '.'
-        #print self.relative_path_
-        tofile = '%s:%s/%s' % (self.pscp_cfg_.svrip,self.pscp_cfg_.svrbasepath,self.relative_path_)
-        tofile = tofile.replace('\\','/')
+        tofile = '%s:%s/%s' % (self.pscp_cfg_.svrip,
+            self.pscp_cfg_.svrbasepath, self.relative_path_)
+        tofile = tofile.replace('\\', '/')
         fromfile = filepath
-        args = self._get_command(fromfile,tofile)
+        args = self._get_command(fromfile, tofile)
         # for a in args:
         #     sublime.message_dialog(a)
         sublime.set_timeout(self.listener.show_status(
             'begin upload file, waiting %ds...' % self.pscp_cfg_.timeout), 0)
-        self.proc = AsyncProcess(args,None,self.listener)
+        self.proc = AsyncProcess(args, None, self.listener)
         self._check_process_finish()
         return True
 
@@ -187,13 +186,13 @@ class PSCPCommand:
                 sublime.set_timeout(self._check_process_finish, 1000)
 
     def _get_pscp_command_path(self):
-        pscp = os.path.join(sublime.packages_path(), 'sftpsync')
+        pscp = os.path.join(sublime.packages_path(), 'SublimeSftpSync')
         if sublime.arch() == "x32":
-            return os.path.join(pscp, 'bin\\pscp.exe')
+            return os.path.join(pscp, 'bin', 'pscp.exe')
         else:
-            return os.path.join(pscp, 'bin\\pscp64.exe')
+            return os.path.join(pscp, 'bin', 'pscp64.exe')
 
-    def _get_command(self,fromfile,tofile):
+    def _get_command(self, fromfile, tofile):
         cmdargs = []
         if sublime.platform() == "windows":
             cmdargs += ['-l', self.pscp_cfg_.svruser, '-P', str(self.pscp_cfg_.svrport)]
@@ -204,18 +203,19 @@ class PSCPCommand:
             else:
                 print "server user password or key file has not been specified"
                 return False
-            
+
             if os.path.isdir(fromfile):
                 cmdargs = [self._get_pscp_command_path()] + cmdargs
                 cmdargs += ['-r', fromfile, tofile]
             else:
                 cmdargs = [self._get_pscp_command_path()] + cmdargs
                 cmdargs += [fromfile, tofile]
-            
+
             #cmdline = cmdline.replace('\\','/')
+            # sublime.message_dialog(self._get_pscp_command_path())
             return cmdargs
         else:
-            cmdline = 'scp -l %s:%d' % (self.pscp_cfg_.svruser,self.pscp_cfg_.svrport)
+            cmdline = 'scp -l %s:%d' % (self.pscp_cfg_.svruser, self.pscp_cfg_.svrport)
             return shlex.split(cmdline)
 
         # p = subprocess.Popen(cmdline,stdin=subprocess.PIPE,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
@@ -228,52 +228,51 @@ class PSCPCommand:
         # else:
         #     self.showResult('Success [%s]' % os.path.basename(filepath))
         #     return True
-        
-    def execDownload(self,filepath):
-        if len(self.relative_path_) == 0:
-            self.relative_path_ = '.'
-            
-        isdir = 0
-        if os.path.isdir(filepath):
-            isdir = 1
-        
-        fromfile = '%s:%s/%s/%s' % (self.pscp_cfg_.svrip,self.pscp_cfg_.svrbasepath,self.relative_path_,os.path.basename(filepath))
-        fromfile = fromfile.replace('\\','/')
-        tofile = os.path.dirname(filepath)
-        if tofile == "":
-            tofile = "."
-        cmdarg = '-l %s -P %d ' % (self.pscp_cfg_.svruser,self.pscp_cfg_.svrport)
-        if len(self.pscp_cfg_.svrkeyfile) > 0:
-            cmdarg = '%s -i %s' % (cmdarg , self.pscp_cfg_.svrkeyfile)
-        elif len(self.pscp_cfg_.svruserpwd) > 0:
-            cmdarg = '%s -pw %s' % (cmdarg , self.pscp_cfg_.svruserpwd)
-        else:
-            print "server user password or key file has not been specified"
-            return False
-        
-        if isdir == 1:
-            cmdline = '%s %s -r %s %s' % (PSCPCMD,cmdarg,fromfile,tofile)
-        else:
-            cmdline = '%s %s %s %s' % (PSCPCMD,cmdarg,fromfile,tofile)
-        print cmdline
-        p = subprocess.Popen(cmdline,stdin=subprocess.PIPE,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
-        ret = p.communicate()
-        if len(ret[0]) == 0:
-            print "result : %s" % ret[1].strip()
-            self.showResult(ret[1].strip())
-            return False
-        else:
-            self.showResult('Success [%s]' % os.path.basename(filepath))
-            return True
-            
-    def execute(self,filepath,upload=True):
+
+    def execDownload(self, filepath):
+        pass
+        # if len(self.relative_path_) == 0:
+        #     self.relative_path_ = '.'
+        # isdir = 0
+        # if os.path.isdir(filepath):
+        #     isdir = 1
+
+        # fromfile = '%s:%s/%s/%s' % (self.pscp_cfg_.svrip,self.pscp_cfg_.svrbasepath,self.relative_path_,os.path.basename(filepath))
+        # fromfile = fromfile.replace('\\','/')
+        # tofile = os.path.dirname(filepath)
+        # if tofile == "":
+        #     tofile = "."
+        # cmdarg = '-l %s -P %d ' % (self.pscp_cfg_.svruser,self.pscp_cfg_.svrport)
+        # if len(self.pscp_cfg_.svrkeyfile) > 0:
+        #     cmdarg = '%s -i %s' % (cmdarg , self.pscp_cfg_.svrkeyfile)
+        # elif len(self.pscp_cfg_.svruserpwd) > 0:
+        #     cmdarg = '%s -pw %s' % (cmdarg , self.pscp_cfg_.svruserpwd)
+        # else:
+        #     print "server user password or key file has not been specified"
+        #     return False
+
+        # if isdir == 1:
+        #     cmdline = '%s %s -r %s %s' % (PSCPCMD, cmdarg, fromfile, tofile)
+        # else:
+        #     cmdline = '%s %s %s %s' % (PSCPCMD, cmdarg, fromfile, tofile)
+        # p = subprocess.Popen(cmdline, stdin=subprocess.PIPE,
+        #     stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        # ret = p.communicate()
+        # if len(ret[0]) == 0:
+        #     print "result : %s" % ret[1].strip()
+        #     self.showResult(ret[1].strip())
+        #     return False
+        # else:
+        #     self.showResult('Success [%s]' % os.path.basename(filepath))
+        #     return True
+
+    def execute(self, filepath, upload=True):
         if upload:
             return self.execUpload(filepath)
         else:
             return self.execDownload(filepath)
-            
-        
-    def transferFile(self,filepath,upload):
+
+    def transferFile(self, filepath, upload):
         if not os.path.isfile(filepath) and not os.path.isdir(filepath):
             sublime.message_dialog("is not a regular file or directory")
             return False
@@ -284,8 +283,4 @@ class PSCPCommand:
             return False
         self.pscp_cfg_.load_config(cfgfile)
         #print "relative[%s]" % self.relative_path_
-        return self.execute(filepath,upload)
-        
-        
-        
-        
+        return self.execute(filepath, upload)
